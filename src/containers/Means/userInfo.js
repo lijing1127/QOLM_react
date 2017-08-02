f170fed9b4b1cdcbccc7a24df51687a86823e782
null_096_8119 <zongwu77@126.com>
2017-08-02 11:33:02 +0800

    响应式修改

--- src/containers/Means/userInfo.js
+++ src/containers/Means/userInfo.js
@@ -1,7 +1,7 @@
 //这是方案室三个页面共用的个人信息组件
 
 import React from 'react';
-import { Icon } from 'antd';
+import { Icon, Row, Col } from 'antd';
 import MeansInfo from 'models/MeansInfo';
 import './userInfo.css';
 import { observer } from 'mobx-react';
@@ -16,19 +16,33 @@ class UserInfo extends React.Component{
 		return (
 			<div className="userInfos">
 				<p><Icon type="user" style={{ fontSize: 18, color: '#37474f' }} />个人信息</p>
-				<div className="row">
-					<div className="col">姓名：{name}</div>
-					<div className="col">民族：{nation}</div>
-					<div className="col">性别：{sex}</div>
-				</div>
-				<div className="row">
-					<div className="col">婚姻状况：{marriage}</div>
-					<div className="col">出生日期：{birthday}</div>
-				</div>	
-				<div className="row">
-					<div className="col">联系方式：{phone}</div>
-					<div className="col">通讯地址：{address}</div>
-				</div>							
+				<Row>
+					<Col xs={{ span: 12, }} lg={{ span: 6 }}>
+						<div className="col">姓名：{name}</div>
+					</Col>
+					<Col xs={{ span: 12, }} lg={{ span: 6 }}>
+						<div className="col">民族：{nation}</div>
+					</Col>
+					<Col xs={{ span: 12, }} lg={{ span: 6 }}>
+						<div className="col">性别：{sex}</div>
+					</Col>
+				</Row>
+				<Row>
+					<Col xs={{ span: 26, }} lg={{ span: 12 }}>
+						<div className="col">婚姻状况：{marriage}</div>
+					</Col>
+					<Col xs={{ span: 26, }} lg={{ span: 12 }}>
+						<div className="col">出生日期：{birthday}</div>
+					</Col>
+				</Row>
+				<Row>
+					<Col xs={{ span: 26, }} lg={{ span: 12 }}>
+						<div className="col">联系方式：{phone}</div>
+					</Col>
+					<Col xs={{ span: 26, }} lg={{ span: 12 }}>
+						<div className="col">通讯地址：{address}</div>
+					</Col>
+				</Row>							
 			</div>
 		);
 	}
